e9e819c705c414a0c71cf1eb1e8cb4acb1289eb3
Nika Shamova <nikashamova@gmail.com>
2017-03-03 14:46:50 +0700

    ABCDEF

--- part1/taskG/taskG.py
+++ part1/taskG/taskG.py
@@ -7,8 +7,12 @@ def create_new_line(line, num):
             cur_sum += word_len + 1
         elif result != "" and result != "\n":
             cur_sum = 0
+            if result != "" and result[len(result) - 1] == " ":
+                result = result[0:len(result) - 1]
             result += "\n"
         result += word + " "
+    if result != "" and result[len(result) - 1] == " ":
+        result = result[0:len(result) - 1]
     return result
 
 
@@ -20,6 +24,7 @@ lines = file_input.readlines()
 
 for line in lines:
     file_output.write(create_new_line(line, num) + "\n")
+    file_output.flush()
 
 
 file_input.close()
