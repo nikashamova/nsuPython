6807092c0f3fc55d683011985eac299fa17e50c0
Nika Shamova <nikashamova@gmail.com>
2017-03-13 13:01:06 +0700

    set + inheritance

---
+++ part2/inheritance.py
@@ -0,0 +1,70 @@
+class Animalia:
+    pass
+
+
+class Chordata(Animalia):
+    pass
+
+
+class Mammalia(Chordata):
+    pass
+
+
+class Carnivora(Mammalia):
+    pass
+
+
+class Felidae(Carnivora):
+    pass
+
+
+class Canidae(Carnivora):
+    pass
+
+
+class Ursidae(Carnivora):
+    pass
+
+
+class Felis(Felidae):
+    pass
+
+
+class Panthera(Felidae):
+    pass
+
+
+class Canis(Canidae):
+    pass
+
+
+class Ursus(Ursidae):
+    pass
+
+
+class Catus(Felis):
+    pass
+
+
+class Pardalis(Felis):
+    pass
+
+
+class Pardus(Panthera):
+    pass
+
+
+class Familiaris(Canis):
+    pass
+
+
+class Lupus(Canis):
+    pass
+
+
+class Arctos(Ursus):
+    pass
+
+
+class Horribilus(Ursus):
+    pass
